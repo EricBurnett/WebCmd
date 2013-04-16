a164d47b58ff4948ace2a0c86d61cff35c5b98bc
Eric Burnett <ericburnett@gmail.com>
2013-04-15 23:32:12 -0400

    Fmt

--- staticcontent/paths.go
+++ staticcontent/paths.go
@@ -1,26 +1,26 @@
 package staticcontent
 
 import (
-    "flag"
 	"encoding/csv"
+	"flag"
 	"io"
 	"log"
 	"os"
 )
 
 var static_content_config = flag.String("static_content_config", "",
-    "Path to the static content config csv file, for auto-configuring custom"+
-    "static paths. See staticcontent/example_paths.csv for examples.")
+	"Path to the static content config csv file, for auto-configuring custom"+
+		"static paths. See staticcontent/example_paths.csv for examples.")
 
 // Adds paths to the static content server based on the shared configuration
 // file. Any paths that cannot be interpreted or found will be ignored.
 func AddCsvPaths(s *Server) error {
-    if len(*static_content_config) == 0 {
-        log.Println("No static content config found; not mapping any "+
-        "directories. Set --static_content_config to have static content "+
-        "hosted.")
-        return nil
-    }
+	if len(*static_content_config) == 0 {
+		log.Println("No static content config found; not mapping any " +
+			"directories. Set --static_content_config to have static content " +
+			"hosted.")
+		return nil
+	}
 	file, err := os.Open(*static_content_config)
 	if err != nil {
 		return err
